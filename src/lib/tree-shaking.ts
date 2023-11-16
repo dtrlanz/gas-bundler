4b3a5a3fc8b8be59f381018ec617c485bd006d7c
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2023-11-16 07:19:55 +0500

    remove features related to tree shaking
    further development (if any) in branch 'globals'

--- src/lib/tree-shaking.ts
+++
@@ -1,4 +0,0 @@
-export function preventTreeShaking(...features: any[]) {
-    // assign to a global dummy variable to prevent tree shaking
-    (globalThis as any).__preventTreeShaking = features;
-}
--- src/tests/server.ts
+++ src/tests/server.ts
@@ -1,8 +1,6 @@
-import { preventTreeShaking } from "../lib/tree-shaking.js";
 import { message } from "./shared.js";
 
-preventTreeShaking(onOpen);
-
+(globalThis as any).onOpen = onOpen;
 function onOpen() {
     const foo = 23;
     const bar = message;
